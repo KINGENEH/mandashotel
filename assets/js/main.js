fea1335856743fc78bbd0cfe85ffbb1013939396
KINGENEH <king.eneh60@gmail.com>
2025-10-31 00:17:20 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,3 +112,14 @@ const sideMenu = document.getElementById("sideMenu");
 menuToggle.addEventListener("click", () => {
   sideMenu.classList.toggle("active");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.getElementById("menuToggle");
+  const sideMenu = document.getElementById("sideMenu");
+
+  if (menuToggle && sideMenu) {
+    menuToggle.addEventListener("click", () => {
+      sideMenu.classList.toggle("active");
+    });
+  }
+});
